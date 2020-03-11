e429d94f030b1d32e8ae82d3639c5e6513b8a68e
Arraytionary <kmkanokpon3@gmail.com>
2020-03-11 18:03:53 +0700

    add euclidean in util, read text data in main and add data-gen function

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main(int argc, char** argv){
+    std::ifstream infile(argv[1]);
+
+    for(int i = 0; i < atoi(argv[3]); i++){
+        float data[atoi(argv[2])];
+        for(int i =0; i < atoi(argv[2]); i++){
+            infile >> data[i];
+        }
+        for(int i=0;i<atoi(argv[2]);i++){
+        std::cout<< data[i]<<std::endl;
+    }
+    }
+    // std::vector<std::vector<float>> data;
+    // std::string temp;
+
+    // while (std::getline(infile, temp)){
+    //     std::istringstream buffer(temp);
+    //     std::vector<float> line((std::istream_iterator<float>(buffer)),
+    //                             std::istream_iterator<float>());
+
+    //     data.push_back(line);
+    // }
+    return 0;
+}
---
+++ util.cpp
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <cmath>
+#include <vector>
+
+float euclideanDistance(std::vector<float> dataA, std::vector<float> dataB){
+    int i = 0;
+    float sumSq = 0;
+    for (std::vector<float>::iterator it = dataA.begin(); it != dataA.end(); ++it){
+        sumSq = pow(*it-dataB[i], 2);
+    }
+    return sqrt(sumSq);
+}
