be9201e4dbdb92248f1365e0866baa119b229f41
Apivich Hemachandra <apivich.hemachandra@gmail.com>
2020-03-19 19:02:50 +0700

    parallel use bulkinsert instead

--- disc_parallel/disc.h
+++ disc_parallel/disc.h
@@ -11,11 +11,11 @@ class DisC {
         float (*distanceFunction)(DataType*, DataType*);
 
         void constructTree() {
-            for (int i = 0; i < this->dataCount; i++) {
-                Object<DataType> *obj = new Object<DataType>(this->data->at(i));
-                this->mTree->insert(obj);
-            }
-//            this->mTree->bulkInsert(data);
+//            for (int i = 0; i < this->dataCount; i++) {
+//                Object<DataType> *obj = new Object<DataType>(this->data->at(i));
+//                this->mTree->insert(obj);
+//            }
+            this->mTree->bulkInsert(data);
         }
 
     public:
