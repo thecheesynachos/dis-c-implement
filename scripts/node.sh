468443dafb7b87d818201ee4f1efec746c5260ce
Apivich Hemachandra <apivich.hemachandra@gmail.com>
2020-03-28 19:12:23 +0700

    script update

--- scripts/node.sh
+++ scripts/node.sh
@@ -2,16 +2,24 @@
 
 for i in 20 40 60 80 100 120 140 160 180 200
 do
-  mkdir -p result/node_result/n$i
+  mkdir -p result/node_result_10k/n$i
+  mkdir -p result/node_result_20k/n$i
   for k in 0 1 2
   do
     # run on 2d, 20k points
-    ./disc_parallel_run data/uniform/d2/s20000/r$k 2 20000 $i 0.2 20 >> result/node_result/n$i/uni_$k
-    ./disc_parallel_run data/uniform/d2/s20000/r$k 2 20000 $i 0.2 20 >> result/node_result/n$i/cls_$k
-    ./disc_bulkinsert_run data/uniform/d2/s20000/r$k 2 20000 $i 0.2 20 >> result/node_result/n$i/uni_$k
-    ./disc_bulkinsert_run data/uniform/d2/s20000/r$k 2 20000 $i 0.2 20 >> result/node_result/n$i/cls_$k
-    ./disc_singleinsert_run data/uniform/d2/s20000/r$k 2 20000 $i 0.2 20 >> result/node_result/n$i/uni_$k
-    ./disc_singleinsert_run data/uniform/d2/s20000/r$k 2 20000 $i 0.2 20 >> result/node_result/n$i/cls_$k
+    ./disc_parallel_run data/uniform/d2/s10000/r$k 2 10000 $i 0.2 20 >> result/node_result_10k/n$i/uni_$k
+    ./disc_parallel_run data/uniform/d2/s10000/r$k 2 10000 $i 0.2 20 >> result/node_result_10k/n$i/cls_$k
+    ./disc_bulkinsert_run data/uniform/d2/s10000/r$k 2 10000 $i 0.2 20 >> result/node_result_10k/n$i/uni_$k
+    ./disc_bulkinsert_run data/uniform/d2/s10000/r$k 2 10000 $i 0.2 20 >> result/node_result_10k/n$i/cls_$k
+    ./disc_singleinsert_run data/uniform/d2/s10000/r$k 2 10000 $i 0.2 20 >> result/node_result_10k/n$i/uni_$k
+    ./disc_singleinsert_run data/uniform/d2/s10000/r$k 2 10000 $i 0.2 20 >> result/node_result_10k/n$i/cls_$k
+    # run on 2d, 20k points
+    ./disc_parallel_run data/uniform/d2/s20000/r$k 2 20000 $i 0.2 20 >> result/node_result_20k/n$i/uni_$k
+    ./disc_parallel_run data/uniform/d2/s20000/r$k 2 20000 $i 0.2 20 >> result/node_result_20k/n$i/cls_$k
+    ./disc_bulkinsert_run data/uniform/d2/s20000/r$k 2 20000 $i 0.2 20 >> result/node_result_20k/n$i/uni_$k
+    ./disc_bulkinsert_run data/uniform/d2/s20000/r$k 2 20000 $i 0.2 20 >> result/node_result_20k/n$i/cls_$k
+    ./disc_singleinsert_run data/uniform/d2/s20000/r$k 2 20000 $i 0.2 20 >> result/node_result_20k/n$i/uni_$k
+    ./disc_singleinsert_run data/uniform/d2/s20000/r$k 2 20000 $i 0.2 20 >> result/node_result_20k/n$i/cls_$k
+    echo $i $k finished
   done
-  echo $i finished
 done
--- scripts/radius.sh
+++ scripts/radius.sh
@@ -2,16 +2,20 @@
 
 for i in $(seq 0.1 0.1 1.0)
 do
-  mkdir -p rad_result_2d/r$i
-  mkdir -p rad_result_5d/r$i
+  mkdir -p rad_result_2d_10k/r$i
+  mkdir -p rad_result_2d_20k/r$i
+  mkdir -p rad_result_4d_10k/r$i
   for k in 0 1 2
   do
     # run on 2d, 10k points
-    ./disc_parallel_run data/uniform/d2/s10000/r$k 2 10000 100 $i 20 >> result/rad_result_2d/r$i/uni_$k
-    ./disc_parallel_run data/clustered/d2/s10000/r$k 2 10000 100 $i 20 >> result/rad_result_2d/r$i/cls_$k
-    # run on 5d, 20k points
-    ./disc_parallel_run data/uniform/d5/s20000/r$k 5 20000 100 $i 20 >> result/ad_result_5d/r$i/uni_$k
-    ./disc_parallel_run data/clustered/d5/s20000/r$k 5 20000 100 $i 20 >> result/rad_result_5d/r$i/cls_$k
+    ./disc_parallel_run data/uniform/d2/s10000/r$k 2 10000 100 $i 20 >> result/rad_result_2d_10k/r$i/uni_$k
+    ./disc_parallel_run data/clustered/d2/s10000/r$k 2 10000 100 $i 20 >> result/rad_result_2d_10k/r$i/cls_$k
+    # run on 2d, 20k points
+    ./disc_parallel_run data/uniform/d2/s20000/r$k 2 20000 100 $i 20 >> result/rad_result_2d_20k/r$i/uni_$k
+    ./disc_parallel_run data/clustered/d2/s20000/r$k 2 20000 100 $i 20 >> result/rad_result_2d_20k/r$i/cls_$k
+    # run on 4d, 10k points
+    ./disc_parallel_run data/uniform/d4/s10000/r$k 4 10000 100 $i 20 >> result/rad_result_4d_10k/r$i/uni_$k
+    ./disc_parallel_run data/clustered/d4/s10000/r$k 4 10000 100 $i 20 >> result/rad_result_4d_10k/r$i/cls_$k
+    echo $i $k finished
   done
-  echo $i finished
 done
