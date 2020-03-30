f932c2ef8831644425c00210b5d4b76a1f91bd60
Apivich Hemachandra <apivich.hemachandra@gmail.com>
2020-03-30 20:19:44 +0700

    update scripts

--- scripts/cores.sh
+++ scripts/cores.sh
@@ -8,11 +8,11 @@ do
   for k in 0 1 2
   do
     # run on 2d, 4k points
-    ./disc_parallel_run data/uniform/d2/s4000/r$k 2 4000 50 0.05 $i >> result/core_result_2d_4k/c$i/unif_$k
-    ./disc_parallel_run data/clustered/d2/s4000/r$k 2 4000 50 0.05 $i >> result/core_result_2d_4k/c$i/clus_$k
+    ./disc_parallel_run data/uniform/d2/s4000/r$k 2 4000 50 0.1 $i >> result/core_result_2d_4k/c$i/unif_$k
+    ./disc_parallel_run data/clustered/d2/s4000/r$k 2 4000 50 0.1 $i >> result/core_result_2d_4k/c$i/clus_$k
     # run on 2d, 8k points
-    ./disc_parallel_run data/uniform/d2/s8000/r$k 2 8000 50 0.05 $i >> result/core_result_2d_8k/c$i/unif_$k
-    ./disc_parallel_run data/clustered/d2/s8000/r$k 2 8000 50 0.05 $i >> result/core_result_2d_8k/c$i/clus_$k
+    ./disc_parallel_run data/uniform/d2/s8000/r$k 2 8000 50 0.1 $i >> result/core_result_2d_8k/c$i/unif_$k
+    ./disc_parallel_run data/clustered/d2/s8000/r$k 2 8000 50 0.1 $i >> result/core_result_2d_8k/c$i/clus_$k
     echo $i $k finished
   done
 done
--- scripts/nodes.sh
+++ scripts/nodes.sh
@@ -7,17 +7,17 @@ do
   for k in 0 1 2
   do
     # run on 2d, 4k points
-    ./disc_parallel_run data/uniform/d2/s4000/r$k 2 4000 $i 0.05 20 >> result/node_result_4k/n$i/uni_$k
-    ./disc_parallel_run data/uniform/d2/s4000/r$k 2 4000 $i 0.05 20 >> result/node_result_4k/n$i/cls_$k
-    ./disc_bulkinsert_run data/uniform/d2/s4000/r$k 2 4000 $i 0.05 20 >> result/node_result_4k/n$i/uni_$k
-    ./disc_bulkinsert_run data/uniform/d2/s4000/r$k 2 4000 $i 0.05 20 >> result/node_result_4k/n$i/cls_$k
-    ./disc_singleinsert_run data/uniform/d2/s4000/r$k 2 4000 $i 0.05 20 >> result/node_result_4k/n$i/uni_$k
-    ./disc_singleinsert_run data/uniform/d2/s4000/r$k 2 4000 $i 0.05 20 >> result/node_result_4k/n$i/cls_$k
+    ./disc_parallel_run data/uniform/d2/s4000/r$k 2 4000 $i 0.1 20 >> result/node_result_4k/n$i/uni_$k
+    ./disc_parallel_run data/uniform/d2/s4000/r$k 2 4000 $i 0.1 20 >> result/node_result_4k/n$i/cls_$k
+    ./disc_bulkinsert_run data/uniform/d2/s4000/r$k 2 4000 $i 0.1 20 >> result/node_result_4k/n$i/uni_$k
+    ./disc_bulkinsert_run data/uniform/d2/s4000/r$k 2 4000 $i 0.1 20 >> result/node_result_4k/n$i/cls_$k
+    ./disc_singleinsert_run data/uniform/d2/s4000/r$k 2 4000 $i 0.1 20 >> result/node_result_4k/n$i/uni_$k
+    ./disc_singleinsert_run data/uniform/d2/s4000/r$k 2 4000 $i 0.1 20 >> result/node_result_4k/n$i/cls_$k
     # run on 2d, 8k points
-    ./disc_parallel_run data/uniform/d2/s8000/r$k 2 8000 $i 0.05 20 >> result/node_result_8k/n$i/uni_$k
-    ./disc_parallel_run data/uniform/d2/s8000/r$k 2 8000 $i 0.05 20 >> result/node_result_8k/n$i/cls_$k
-    ./disc_bulkinsert_run data/uniform/d2/s8000/r$k 2 8000 $i 0.05 20 >> result/node_result_8k/n$i/uni_$k
-    ./disc_bulkinsert_run data/uniform/d2/s8000/r$k 2 8000 $i 0.05 20 >> result/node_result_8k/n$i/cls_$k
+    ./disc_parallel_run data/uniform/d2/s8000/r$k 2 8000 $i 0.1 20 >> result/node_result_8k/n$i/uni_$k
+    ./disc_parallel_run data/uniform/d2/s8000/r$k 2 8000 $i 0.1 20 >> result/node_result_8k/n$i/cls_$k
+    ./disc_bulkinsert_run data/uniform/d2/s8000/r$k 2 8000 $i 0.1 20 >> result/node_result_8k/n$i/uni_$k
+    ./disc_bulkinsert_run data/uniform/d2/s8000/r$k 2 8000 $i 0.1 20 >> result/node_result_8k/n$i/cls_$k
     ./disc_singleinsert_run data/uniform/d2/s8000/r$k 2 8000 $i 0.05 20 >> result/node_result_8k/n$i/uni_$k
     ./disc_singleinsert_run data/uniform/d2/s8000/r$k 2 8000 $i 0.05 20 >> result/node_result_8k/n$i/cls_$k
     echo $i $k finished
