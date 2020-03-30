8e5de1978add98eebc18d661524e54a106860b02
Apivich Hemachandra <apivich.hemachandra@gmail.com>
2020-03-30 19:54:36 +0700

    update scripts

--- scripts/cores.sh
+++ scripts/cores.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-for i in {2..2..20}
+for i in 2 4 6 8 10 12 14 16 18 20
 do
-  mkdir -p result/core_result_2d_10k/c$i
-  mkdir -p result/core_result_2d_20k/c$i
-#  mkdir -p result/core_result_4d_10k/c$i
+  mkdir -p result/core_result_2d_4k/c$i
+  mkdir -p result/core_result_2d_8k/c$i
+#  mkdir -p result/core_result_4d_4k/c$i
   for k in 0 1 2
   do
-    # run on 2d, 10k points
-    ./disc_parallel_run data/uniform/d2/s10000/r$k 2 10000 100 0.2 $i >> result/core_result_2d_10k/c$i/unif_$k
-    ./disc_parallel_run data/clustered/d2/s10000/r$k 2 10000 100 0.2 $i >> result/core_result_2d_10k/c$i/clus_$k
-    # run on 2d, 20k points
-    ./disc_parallel_run data/uniform/d2/s20000/r$k 2 20000 100 0.2 $i >> result/core_result_2d_20k/c$i/unif_$k
-    ./disc_parallel_run data/clustered/d2/s20000/r$k 2 20000 100 0.2 $i >> result/core_result_2d_20k/c$i/clus_$k
-#    # run on 4d, 10k points
-#    ./disc_parallel_run data/uniform/d4/s10000/r$k 4 10000 100 0.2 $i >> result/core_result_4d_10k/c$i/unif_$k
-#    ./disc_parallel_run data/clustered/d5/s10000/r$k 4 10000 100 0.2 $i >> result/core_result_4d_10k/c$i/clus_$k
+    # run on 2d, 4k points
+    ./disc_parallel_run data/uniform/d2/s4000/r$k 2 4000 50 0.05 $i >> result/core_result_2d_4k/c$i/unif_$k
+    ./disc_parallel_run data/clustered/d2/s4000/r$k 2 4000 50 0.05 $i >> result/core_result_2d_4k/c$i/clus_$k
+    # run on 2d, 8k points
+    ./disc_parallel_run data/uniform/d2/s8000/r$k 2 8000 50 0.05 $i >> result/core_result_2d_8k/c$i/unif_$k
+    ./disc_parallel_run data/clustered/d2/s8000/r$k 2 8000 50 0.05 $i >> result/core_result_2d_8k/c$i/clus_$k
     echo $i $k finished
   done
 done
--- scripts/radius.sh
+++ scripts/radius.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-for i in $(seq 0.2 0.2 1.0)
+for i in 0.02 0.04 0.06 0.08 0.1 0.12 0.14 0.16 0.18 0.2
 do
-  mkdir -p result/rad_result_2d_10k/r$i
-  mkdir -p result/rad_result_2d_20k/r$i
+  mkdir -p result/rad_result_2d_4k/r$i
+  mkdir -p result/rad_result_2d_8k/r$i
 #  mkdir -p result/rad_result_4d_10k/r$i
   for k in 0 1 2
   do
-    # run on 2d, 10k points
-    ./disc_parallel_run data/uniform/d2/s10000/r$k 2 10000 100 $i 20 >> result/rad_result_2d_10k/r$i/uni_$k
-    ./disc_parallel_run data/clustered/d2/s10000/r$k 2 10000 100 $i 20 >> result/rad_result_2d_10k/r$i/cls_$k
-    # run on 2d, 20k points
-    ./disc_parallel_run data/uniform/d2/s20000/r$k 2 20000 100 $i 20 >> result/rad_result_2d_20k/r$i/uni_$k
-    ./disc_parallel_run data/clustered/d2/s20000/r$k 2 20000 100 $i 20 >> result/rad_result_2d_20k/r$i/cls_$k
-#    # run on 4d, 10k points
-#    ./disc_parallel_run data/uniform/d4/s10000/r$k 4 10000 100 $i 20 >> result/rad_result_4d_10k/r$i/uni_$k
-#    ./disc_parallel_run data/clustered/d4/s10000/r$k 4 10000 100 $i 20 >> result/rad_result_4d_10k/r$i/cls_$k
+    # run on 2d, 4k points
+    ./disc_parallel_run data/uniform/d2/s4000/r$k 2 4000 50 $i 20 >> result/rad_result_2d_4k/r$i/uni_$k
+    ./disc_parallel_run data/clustered/d2/s4000/r$k 2 4000 50 $i 20 >> result/rad_result_2d_4k/r$i/cls_$k
+    # run on 2d, 8k points
+    ./disc_parallel_run data/uniform/d2/s8000/r$k 2 8000 50 $i 20 >> result/rad_result_2d_8k/r$i/uni_$k
+    ./disc_parallel_run data/clustered/d2/s8000/r$k 2 8000 50 $i 20 >> result/rad_result_2d_8k/r$i/cls_$k
     echo $i $k finished
   done
 done
